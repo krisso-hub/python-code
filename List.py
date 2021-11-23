f0f5c58a5d1236f04dff42a5c2a720236811202e
krisso <zozopapa804@gmail.com>
2021-11-23 11:40:43 -0500

    list of multiples added

--- List.py
+++ List.py
@@ -1,10 +1,16 @@
+# cube list of numbers and print the output
 list_numbers = []
-numbers = list(range(3, 31))
+numbers = list(range(3, 31, 3))
 for number in numbers:
     list_numbers.append(number**3)
 
 print(list_numbers)
 
-
+# A list of multiples of three(3)
+list_multiples = []
+num = list(range(3, 30))
+for number in num:
+    list_multiples.append(number*3)
+print(list_multiples)
 
 

81bdc39c52f7ce84b4b3303612a9c9a8816c739b
krisso <zozopapa804@gmail.com>
2021-12-17 17:52:31 -0500

    populating a list with a dict

--- dict-loop.py
+++ dict-loop.py
@@ -1,27 +1,9 @@
 
-user_01 = {
-    'username': 'femi-bobo',
-    'fname': 'femi',
-    'lname': 'dana'
-}
+aliens = []
 
-for key, value in user_01.items():
-    print(f'\nkey: {key}')
-    print(f'value: {value}')
+for alien_number in range(30):
+    new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
+    aliens.append(new_alien)
 
-favourite_langauge = {
-    'james': 'python',
-    'john': 'nodejs',
-    'jane': 'Go',
-    'janet': 'java',
-    'johnson': 'javascript'
-}
-
-for name, lang in favourite_langauge.items():
-    # print(f'\n the favorite languge of {name.title()} is {lang.title()}')
-    print(f"\n{name.title()}'s favorite language is {lang.title()}")
-
-for name in favourite_langauge.keys():
-    print(f'\n hi {name.title()}')
-
-    
+for alien in aliens[:5]:
+    print(alien)

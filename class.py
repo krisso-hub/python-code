3530b0c3a81c16bcb0f5ed43784e9b8a01a5c665
krisso <zozopapa804@gmail.com>
2021-12-30 07:06:15 -0500

    modeling a restaruant class

---
+++ class.py
@@ -0,0 +1,20 @@
+# modeling a restarunat
+class Restaruant:
+# initializes the restaruant name and cuisine
+    def __init__(self, name, cuisine_type):
+        self.name = name
+        self.cuisine_type = cuisine_type
+
+# describe the location and kind of cuisine
+    def describe_restaruant(self):
+        print(f"The {self.name} is on the east side")
+        print(f"The {self.name} has a classic {self.cuisine_type}")
+
+# shows the hours of operation
+    def open_restaurant(self):
+        print(f"The {self.name} is opened from 8am to 10pm")
+
+her_restaruant = Restaruant("Delicious Restarunat", "french cuisine")
+print(f"{her_restaruant.name} is a place to visit")
+print(f"The restaruant is a {her_restaruant.cuisine_type}")
+her_restaruant.describe_restaruant()
--- funct.py
+++ funct.py
@@ -1,4 +1,5 @@
-
+# accepting n number of arguments in a function
+#
 def make_pizza(size, *toppings):
     print(f'\nMaking a {size}-inch pizza with the following toppings:')
     for topping in toppings:

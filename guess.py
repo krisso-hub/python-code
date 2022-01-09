181c820c22aec02e44bc364509f02e4ad77e0a9d
krisso <zozopapa804@gmail.com>
2022-01-09 16:18:38 -0500

    guess added

---
+++ guess.py
@@ -0,0 +1,19 @@
+import random
+
+secret_number = random.randint(1, 30)
+
+for guess_taken in range(1, 5):
+    print("Guess a secret number")
+    guess = int(input())
+
+    if guess < secret_number:
+        print('your guess number is low, try again')
+
+    elif guess > secret_number:
+        print('your guess number is high, try again')
+    elif guess == secret_number:
+        print(f'Yaa! you got the correct number {secret_number} in {guess_taken} guesses')
+    else:
+        print(f'Naa! You did not get the correct number in {guess_taken} guesses')
+
+print(f'the secret number is {secret_number} and your number of attemps is exhausted, start all over')
